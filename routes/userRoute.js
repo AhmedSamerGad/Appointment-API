77297315b4a93756529684f3e9f9a1083421ce89
A.S <AhmedSamerGad>
2025-06-18 13:31:23 +0300

    add the profile image to cloudinary

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import * as userController from '../controllers/userController.js';
+import {uploadSingleImage} from "../middlewares/multer.js";
 
 const userRoute = express.Router();
-userRoute.post('/regist',userController.registerUser).post('/login',userController.loginUser).get('/profile',userController.getUserProfile);
+userRoute.post('/regist',uploadSingleImage('profilePic'),userController.registerUser).post('/login',userController.loginUser).get('/profile',userController.getUserProfile);
 userRoute.patch(':id',userController.updateUserProfile).delete(':id',userController.deleteUser);
 export default userRoute;
