0c8ed9a367dbf856ae34704744007a13f8cad72f
A.S <AhmedSamerGad>
2025-07-02 21:02:22 +0300

    solve some deployment errors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({origin : '*'  , methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],} ,));
 
 app.use('/api/v1/appointment',appointmentRoute);
 app.use('/api/v1/user',userRoute);
